4a3cb7e9a7e9716b47818239c07203ef86e79718
Globa <Globa@DESKTOP-E96IGNL>
2020-12-13 15:23:01 +0300

    Добавьте файлы проекта.

---
+++ Дифференцирование/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace Дифференцирование
+{
+    partial class FunctionGraphic
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FunctionGraphic));
+            this.Calculate = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Info = new System.Windows.Forms.TextBox();
+            this.FunctionBox = new System.Windows.Forms.ComboBox();
+            this.ImageOfFunction = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FunctionsIMG = new System.Windows.Forms.ImageList(this.components);
+            this.Table = new System.Windows.Forms.DataGridView();
+            this.xValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.k0Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.k1Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.k2Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.k3Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.yValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Graphic = new LiveCharts.WinForms.CartesianChart();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageOfFunction)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Table)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(12, 184);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(278, 40);
+            this.Calculate.TabIndex = 0;
+            this.Calculate.Text = "Рассчитать";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.Info);
+            this.panel1.Controls.Add(this.FunctionBox);
+            this.panel1.Controls.Add(this.ImageOfFunction);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(278, 166);
+            this.panel1.TabIndex = 1;
+            // 
+            // Info
+            // 
+            this.Info.Location = new System.Drawing.Point(27, 89);
+            this.Info.Multiline = true;
+            this.Info.Name = "Info";
+            this.Info.Size = new System.Drawing.Size(219, 63);
+            this.Info.TabIndex = 3;
+            // 
+            // FunctionBox
+            // 
+            this.FunctionBox.FormattingEnabled = true;
+            this.FunctionBox.Items.AddRange(new object[] {
+            "Функция №1",
+            "Функция №2",
+            "Функция №3",
+            "Функция №4",
+            "Функция №5"});
+            this.FunctionBox.Location = new System.Drawing.Point(122, 8);
+            this.FunctionBox.Name = "FunctionBox";
+            this.FunctionBox.Size = new System.Drawing.Size(143, 21);
+            this.FunctionBox.TabIndex = 0;
+            this.FunctionBox.SelectedIndexChanged += new System.EventHandler(this.FunctionBox_SelectedIndexChanged);
+            // 
+            // ImageOfFunction
+            // 
+            this.ImageOfFunction.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ImageOfFunction.Location = new System.Drawing.Point(63, 35);
+            this.ImageOfFunction.MaximumSize = new System.Drawing.Size(151, 48);
+            this.ImageOfFunction.MinimumSize = new System.Drawing.Size(151, 48);
+            this.ImageOfFunction.Name = "ImageOfFunction";
+            this.ImageOfFunction.Size = new System.Drawing.Size(151, 48);
+            this.ImageOfFunction.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.ImageOfFunction.TabIndex = 2;
+            this.ImageOfFunction.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выбирете функцию:";
+            // 
+            // FunctionsIMG
+            // 
+            this.FunctionsIMG.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("FunctionsIMG.ImageStream")));
+            this.FunctionsIMG.TransparentColor = System.Drawing.Color.Transparent;
+            this.FunctionsIMG.Images.SetKeyName(0, "Function1.png");
+            this.FunctionsIMG.Images.SetKeyName(1, "Function2.PNG");
+            this.FunctionsIMG.Images.SetKeyName(2, "Function3.PNG");
+            this.FunctionsIMG.Images.SetKeyName(3, "Function4.PNG");
+            this.FunctionsIMG.Images.SetKeyName(4, "Function5.PNG");
+            // 
+            // Table
+            // 
+            this.Table.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.Table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Table.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.xValue,
+            this.k0Value,
+            this.k1Value,
+            this.k2Value,
+            this.k3Value,
+            this.yValue});
+            this.Table.Location = new System.Drawing.Point(296, 12);
+            this.Table.Name = "Table";
+            this.Table.Size = new System.Drawing.Size(380, 212);
+            this.Table.TabIndex = 2;
+            // 
+            // xValue
+            // 
+            this.xValue.HeaderText = "X";
+            this.xValue.Name = "xValue";
+            // 
+            // k0Value
+            // 
+            this.k0Value.HeaderText = "k0";
+            this.k0Value.Name = "k0Value";
+            // 
+            // k1Value
+            // 
+            this.k1Value.HeaderText = "k1";
+            this.k1Value.Name = "k1Value";
+            // 
+            // k2Value
+            // 
+            this.k2Value.HeaderText = "k2";
+            this.k2Value.Name = "k2Value";
+            // 
+            // k3Value
+            // 
+            this.k3Value.HeaderText = "k3";
+            this.k3Value.Name = "k3Value";
+            // 
+            // yValue
+            // 
+            this.yValue.HeaderText = "Y";
+            this.yValue.Name = "yValue";
+            // 
+            // Graphic
+            // 
+            this.Graphic.Location = new System.Drawing.Point(12, 230);
+            this.Graphic.Name = "Graphic";
+            this.Graphic.Size = new System.Drawing.Size(664, 202);
+            this.Graphic.TabIndex = 3;
+            // 
+            // FunctionGraphic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(688, 436);
+            this.Controls.Add(this.Graphic);
+            this.Controls.Add(this.Table);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Calculate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "FunctionGraphic";
+            this.Text = "Дифференцирование";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageOfFunction)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Table)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox Info;
+        private System.Windows.Forms.PictureBox ImageOfFunction;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox FunctionBox;
+        private System.Windows.Forms.ImageList FunctionsIMG;
+        private System.Windows.Forms.DataGridView Table;
+        private System.Windows.Forms.DataGridViewTextBoxColumn xValue;
+        private System.Windows.Forms.DataGridViewTextBoxColumn k0Value;
+        private System.Windows.Forms.DataGridViewTextBoxColumn k1Value;
+        private System.Windows.Forms.DataGridViewTextBoxColumn k2Value;
+        private System.Windows.Forms.DataGridViewTextBoxColumn k3Value;
+        private System.Windows.Forms.DataGridViewTextBoxColumn yValue;
+        private LiveCharts.WinForms.CartesianChart Graphic;
+    }
+}
+
---
+++ Дифференцирование/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LiveCharts.Wpf;
+
+namespace Дифференцирование
+{
+    public partial class FunctionGraphic : Form
+    {
+        private const double h = 0.05f;
+
+        private Func<double, double, double> function;
+        private List<string> xValues;
+        private LiveCharts.ChartValues<double> yValues;
+        private double x, y, b, k0, k1, k2, k3;
+
+        public FunctionGraphic()
+        {
+            InitializeComponent();
+        }
+
+        private void FunctionBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Info.Clear();
+            int selectedFunction = FunctionBox.SelectedIndex;
+            switch (selectedFunction)
+            {
+                case 0:
+                    SetFunction(Function1);
+                    SetFunctionInfo(1.0, 1.0, 2.0);
+                    SetImageOfFunction(0);
+                    break;
+                case 1:
+                    SetFunction(Function2);
+                    SetFunctionInfo(0.0, 1.0, 1.0);
+                    SetImageOfFunction(1);
+                    break;
+                case 2:
+                    SetFunction(Function3);
+                    SetFunctionInfo(0.0, 1.0, 1.0);
+                    SetImageOfFunction(2);
+                    break;
+                case 3:
+                    SetFunction(Function4);
+                    SetFunctionInfo(3.0, 3.0, 4.0);
+                    SetImageOfFunction(3);
+                    break;
+                case 4:
+                    SetFunction(Function5);
+                    SetFunctionInfo(0.0, 1.0, 1.0);
+                    SetImageOfFunction(4);
+                    break;
+            }
+            SetDefaults();
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            SetDefaults();
+            SolveEquation();
+        }
+
+        private void SetDefaults()
+        {
+            xValues = new List<string>();
+            yValues = new LiveCharts.ChartValues<double>();
+            k0 = 0; k1 = 0; k2 = 0; k3 = 0;
+
+            Table.Rows.Clear();
+            Table.Update();
+
+            Graphic.AxisX.Clear();
+            Graphic.AxisY.Clear();
+        }
+
+        private void SolveEquation()
+        {
+            if (function == null) return;
+            while (x <= b + h)
+            {
+                SendValuesToTable();
+                CalculateValues();
+                TakeStep();
+            }
+            SendValuesToGraphic();
+        }
+
+        private void SendValuesToTable()
+        {
+            Table.Rows.Add(x, k0, k1, k2, k3, y);
+            xValues.Add(Math.Round(x,2).ToString());
+            yValues.Add(Math.Round(y,2));
+        }
+
+        private void CalculateValues()
+        {
+            k0 = h * function(x, y);
+            k1 = h * function(x + h / 2.0, y + k0 / 2.0);
+            k2 = h * function(x + h / 2.0, y + k1 / 2.0);
+            k3 = h * function(x + h, y + k2);
+            y += 1.0 / 6.0 * (k0 + 2.0 * k1 + 2.0 * k2 + k3);
+        }
+
+        private void TakeStep()
+        {
+            x += h;
+        }
+
+        private void SendValuesToGraphic()
+        {
+            LiveCharts.SeriesCollection seriesCollection = new LiveCharts.SeriesCollection();
+
+            Graphic.AxisX.Add(new Axis()
+            {
+                Labels = xValues
+            });
+
+            LineSeries line = new LineSeries();
+            line.Title = "Y";
+            line.Values = yValues;
+            seriesCollection.Add(line);
+
+            Graphic.Series = seriesCollection;
+        }
+
+        private void SetFunction(Func<double, double, double> function)
+        {
+            this.function = function;
+        }
+
+        private void SetFunctionInfo(double x, double y, double b)
+        {
+            this.x = x; this.b = b; this.y = y;
+            AddLine(Info, $"X0 = {x}");
+            AddLine(Info, $"Y0 = {y}");
+            AddLine(Info, $"B = {b}");
+            AddLine(Info, $"H = {Math.Round(h,2)}");
+        }
+
+        private void SetImageOfFunction(int index)
+        {
+            ImageOfFunction.Image = FunctionsIMG.Images[index];
+        }
+
+        private void AddLine(TextBox textBox, string line)
+        {
+            textBox.Text += line;
+            textBox.Text += "\r\n";
+        }
+
+        private double Function1(double x, double y)
+        {
+            return x * Math.Pow(Math.Sin(y), 2.0);
+        }
+
+        private double Function2(double x, double y)
+        {
+            return x/Math.Sqrt(2)*y;
+        }
+
+        private double Function3(double x, double y)
+        {
+            return 2*x*Math.Sqrt(y);
+        }
+
+        private double Function4(double x, double y)
+        {
+            return Math.Sqrt(x*y);
+        }
+
+        private double Function5(double x, double y)
+        {
+            return y * Math.Cos(x);
+        }
+    }
+}
